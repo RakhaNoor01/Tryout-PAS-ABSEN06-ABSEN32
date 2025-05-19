d7b779bd186a394168d350dc0c768fdc63b6d37c
RakhaNoor01 <05530@smkrus.sch.id>
2025-05-19 14:45:55 +0700

    Sudah sayang 💖💖💖

--- app/src/main/java/com/example/tryout_pas/SportsDBApi.java
+++ app/src/main/java/com/example/tryout_pas/SportsDBApi.java
@@ -5,4 +5,7 @@ import retrofit2.http.GET;
 public interface SportsDBApi {
     @GET("api/v1/json/3/search_all_teams.php?l=English%20Premier%20League\n")
     Call<TeamResponse> getTeams();
+
+    @GET("/api/v1/json/3/search_all_teams.php?s=Soccer&c=Spain\n")
+    Call<TeamResponse> getTeams2();
 }
